df34a529ddf81329dbd354e38c452068f6718e7c
fouad <fouad@gmail.com>
2022-09-10 23:54:23 +0100

    I added changes to the challenge 11(2)

--- semaine 1/challenge 11(2)/main.c
+++ semaine 1/challenge 11(2)/main.c
@@ -4,33 +4,24 @@
 int main()
 {
     int h1,m1,s1,h2,m2,s2;
-    printf("    enter the moment 1:\n");
-      printf("hour: ");
-    scanf("%d",&h1);
-      printf("minute: ");
-    scanf("%d",&m1);
-      printf("second: ");
-    scanf("%d",&s1);
+    printf("    enter the moment 1 in this formula (HH:MM:SS) :  ");
 
-    printf("    enter the moment 2:\n");
-      printf("hour: ");
-    scanf("%d",&h2);
-      printf("minute: ");
-    scanf("%d",&m2);
-      printf("second: ");
-    scanf("%d",&s2);
-    printf("moment 1 is %d : %d : %d",h1,m1,s1);
-    printf("\nmoment 2 is %d : %d : %d\n",h2,m2,s2);
-    if(h1>h2) printf("the moment 1 comes before the moment 2. ");
-        else if(h1<h2)
+     scanf("%d:%d:%d",&h1,&m1,&s1);
+
+
+    printf("    enter the moment 2:  ");
+    scanf("%d:%d:%d",&h2,&m2,&s2);
+
+    if(h1<h2) printf("the moment 1 comes before the moment 2. ");
+        else if(h1>h2)
         printf("the moment 1 comes after the moment 2. ");
-    else{if(m1>m2)
+    else{if(m1<m2)
           printf("the moment 1 comes before the moment 2. ");
-        else if(m1<m2)
+        else if(m1>m2)
           printf("the moment 1 comes after the moment 2. ");
-         else{if(s1>s2)
+         else{if(s1<s2)
             printf("the moment 1 comes before the moment 2. ");
-             else if(s1<s2)
+             else if(s1>s2)
             printf("the moment 1 comes after the moment 2. ");
             else{printf("the moments are the same.");}
          }
